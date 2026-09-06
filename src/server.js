669690933fbb6ccee41e56b3318f1d29ac706793
Liliia Risina <risinalilia2@gmail.com>
2026-09-06 12:39:46 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,4 +4,7 @@ dotenv.config();
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(` Running http://localhost:${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
